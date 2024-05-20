cd360048d7dfb0a75a040d1b1ccbc0ec2b2c5cf7
dragutmario <150171642+dragutmario@users.noreply.github.com>
2024-05-20 17:44:30 +0300

    Add files via upload

---
+++ assets/js/TrendingCard.js
@@ -0,0 +1,19 @@
+function TrendingCard({ id, title, trendTitle, description, image, category }) {
+    return `
+        <div class = "trending__product swiper-slide" data-id='${id}' data-category='${category}'>
+            <div class='trending__product__image'>
+                <img src='${image}' alt='${title}'/>
+            </div>
+            <div class="trending__product__description">
+                <div class="trending__product__text">
+                    <h1 class="title">
+                        <span>${trendTitle.split(" ")[0]}</span>
+                        <p>${trendTitle.split(" ").slice(1).join(" ")}</p>
+                    </h1>
+                    <p class="desc">${description}</p>
+                </div>
+        </div>
+    `;
+}
+
+export default TrendingCard;
